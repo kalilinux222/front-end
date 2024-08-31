0bce9ef5c5e5b307e39201abdae5989518410fab
Syed Abuthahir Mohamed Yusuf <syedabuthahir.mohamedyusuf@aroopatech.com>
2024-08-31 10:27:00 +0530

    add product list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/admin-dashboard/sidebar";
+import ViewProducts from "./components/shopping-page/view-products";
 function App() {
   return (
     <div>
       <Router>
         <Routes>
           <Route path="/admin-dashboard" element={<Sidebar />} />
+          <Route path="/" element={<ViewProducts />} />
         </Routes>
       </Router>
     </div>
